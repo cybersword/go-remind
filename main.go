ac4759e6fb78c06b4dd3117e815c92b911a349dc
WANG YU <liuleidejian@gmail.com>
2016-11-11 01:20:43 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HelloServer just say hello
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Inside HelloServer handler")
+	fmt.Fprintf(w, "Hello,"+req.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe("localhost:8765", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}

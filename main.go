1fa5bfb2bc6d3f8cebcdb0e5e3541f5e8734325e
cybersword <liuleidejian@gmail.com>
2016-11-16 03:23:59 +0800

    格式化代码

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"io"
 	"io/ioutil"
 	"log"
-	"encoding/json"
 	"net/http"
 )
 
 // helloHandle just say hello
 func helloHandle(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Inside HelloServer handler")
+	fmt.Fprintln(w, "<html><body>")
 	fmt.Fprintf(w, "Hello, "+req.URL.Path[1:]+"\n")
-
-	io.WriteString(w, "<h1>hello, world</h1>")
+	io.WriteString(w, "<h1>hello, world</h1>\n")
+	fmt.Fprintln(w, "</body></html>")
 }
 
 func indexHandle(w http.ResponseWriter, req *http.Request) {
@@ -23,20 +24,20 @@ func indexHandle(w http.ResponseWriter, req *http.Request) {
 	case "GET":
 		io.WriteString(w, `{"code": 0, "msg": "GET"}`)
 	case "POST":
-		   contentType := req.Header.Get("Content-Type")
-		   io.WriteString(w, contentType+"\n")
-		   switch contentType {
-		   case "application/json":
-var mapBody map[string]interface{}
-body, _ := ioutil.ReadAll(req.Body)
-json.Unmarshal(body, &mapBody)
-		   io.WriteString(w, mapBody["user"].(string))
-		   default:
-req.ParseForm();
-        io.WriteString(w, req.Form["user"][0])
-		io.WriteString(w, req.FormValue("plan"))
-			   
-		   }
+		contentType := req.Header.Get("Content-Type")
+		io.WriteString(w, contentType+"\n")
+
+		switch contentType {
+		case "application/json":
+			var mapBody map[string]interface{}
+			body, _ := ioutil.ReadAll(req.Body)
+			json.Unmarshal(body, &mapBody)
+			io.WriteString(w, mapBody["user"].(string))
+		default:
+			req.ParseForm()
+			io.WriteString(w, req.Form["user"][0])
+			io.WriteString(w, req.FormValue("plan"))
+		}
 
 	}
 }

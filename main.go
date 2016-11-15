e5c514234ae65f69670bcd2fcb2350d4c78e3535
WANG YU <liuleidejian@gmail.com>
2016-11-16 01:31:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,26 +3,39 @@ package main
 import (
 	"fmt"
 	"html"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/cybersword/string"
 )
 
-// HelloServer just say hello
-func HelloServer(w http.ResponseWriter, req *http.Request) {
+// helloHandle just say hello
+func helloHandle(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Inside HelloServer handler")
 	fmt.Fprintf(w, "Hello, "+req.URL.Path[1:]+"\n")
-	s := req.URL.Path[1:]
-	fmt.Fprintf(w, "reverse: "+string.Reverse(s))
+
+	io.WriteString(w, "<h1>hello, world</h1>")
+}
+
+func indexHandle(w http.ResponseWriter, req *http.Request) {
+	switch request.Method {
+	case "GET":
+		io.WriteString(w, `{"code": 0, "msg": "GET"}`)
+	case "POST":
+        /* handle the form data, note that ParseForm must
+           be called before we can extract form data */
+        	req.ParseForm();
+        	io.WriteString(w, req.Form["user"])
+		io.WriteString(w, request.FormValue("plan"))
+	}
 }
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/hello", helloHandle)
 	// 最长匹配原则
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	http.HandleFunc("/", indexHandle)
 	err := http.ListenAndServe("localhost:8765", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())

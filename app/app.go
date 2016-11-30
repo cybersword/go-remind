f5a0374f2e6cdf8ef39e143e826c3aa0d15c0e3e
cybersword <liuleidejian@gmail.com>
2016-11-30 12:17:55 +0800

    import

--- app/app.go
+++ app/app.go
@@ -1,19 +1,22 @@
 package app
 
 import (
-	"log"
+	"github.com/cybersword/go-remind/utils"
 )
 
+// App entry
 type App struct {
 	Name string
 }
 
+// Plan controller
 func (a *App) Plan(params map[string]interface{}) (int, string, interface{}) {
-	log.Println("call Plan")
+	utils.Notice("call Plan")
 	return 0, a.Name + "call plan", params
 }
 
+// Task controller
 func (a *App) Task(params map[string]interface{}) (int, string, interface{}) {
-	log.Println("call Task")
+	utils.Notice("call Task")
 	return 0, a.Name + "call task", params
 }
